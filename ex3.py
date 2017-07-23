84285af71614b1649e1be4bcb8397cfc37613820
molBR <pedromollica@gmail.com>
2017-07-23 18:08:47 +0000

    ex3 corrigido

--- ex3.py
+++ ex3.py
@@ -5,55 +5,55 @@ import struct
 from ctypes import *
 
 # host ot listen on
-host = "192.168.0.108"
+host = "192.168.1.6"
 
 
 class ICMP(Structure):
-    _fields_ = [
-        ("type",            c_ubyte),
-        ("code",            c_ubyte),
-        ("checksum",        c_ushort),
-        ("unused",          c_ushort),
-        ("next_hop_mtu",    c_ushort)
-    ]
-    def __new__(self,socket_buffer):
-        return self.from_buffer_copy(socket_buffer)
+	_fields_ = [
+	("type",            c_ubyte),
+	("code",            c_ubyte),
+	("checksum",        c_ushort),
+	("unused",          c_ushort),
+	("next_hop_mtu",    c_ushort)
+	]
+	def __new__(self,socket_buffer):
+		return self.from_buffer_copy(socket_buffer)
 
-    def __init__(self, socket_buffer):
-        pass
+	def __init__(self, socket_buffer):
+		pass
 # our IP header
 class IP(Structure):
-    _fields_= [
-        ("ihl",             c_ubyte, 4),
-        ("version",         c_ubyte, 4),
-        ("tos",             c_ubyte),
-        ("len",             c_ushort),
-        ("id",              c_ushort),
-        ("offset",          c_ushort),
-        ("ttl",             c_ubyte),
-        ("protocol_num",    c_ubyte),
-        ("sum",             c_ushort),
-        ("src",             c_ulong),
-        ("dst",             c_ulong)
-    ]
+	_fields_= [
+	("ihl",             c_ubyte, 4),
+	("version",         c_ubyte, 4),
+	("tos",             c_ubyte),
+	("len",             c_ushort),
+	("id",              c_ushort),
+	("offset",          c_ushort),
+	("ttl",             c_ubyte),
+	("protocol_num",    c_ubyte),
+	("sum",             c_ushort),
+	("src",             c_int32),
+	("dst",             c_int32)
+	]
 
-def __new__(self, socket_buffer=None):
-    return self.from_buffer_copy(socket_buffer)
+	def __new__(self, socket_buffer=None):
+	    return self.from_buffer_copy(socket_buffer)
 
-def __init__(self, socket_buffer=None):
+	def __init__(self, socket_buffer=None):
 
-    # map protocol constants to their names
-    self.protocol_map = {1: "ICMP", 6:"TCP", 17:"UDP"}
+	    # map protocol constants to their names
+	    self.protocol_map = {1: "ICMP", 6:"TCP", 17:"UDP"}
 
-    # human readable IP addresses
-    self.src_address = socket.inet_ntoa(struct.pack("<L",self.src))
-    self.dst_address = socket.inet_ntoa(struct.pack("<L",self.dst))
+	    # human readable IP addresses
+	    self.src_address = socket.inet_ntoa(struct.pack("@I",self.src))
+	    self.dst_address = socket.inet_ntoa(struct.pack("@I",self.dst))
 
-    # human readable protocol
-    try:
-        self.protocol = self.protocol_map[self.protocol_num]
-    except:
-        self.protocol = str(self.protocol_num)
+	    # human readable protocol
+	    try:
+		self.protocol = self.protocol_map[self.protocol_num]
+	    except:
+		self.protocol = str(self.protocol_num)
 
 #this should look familiar to sniffy.py
 if os.name == "nt":
@@ -77,12 +77,13 @@ try:
         raw_buffer = sniffer.recvfrom(65565)[0]
 
         #Create an IP header from the first 20 bytes of the buffer
-        ip_header = IP(raw_buffer[0:20])
+        ip_header = IP(raw_buffer)
 
         #Print out the protocol that was detected and the hosts
         print ("Protocol: %s %s -> %s" % (ip_header.protocol,
         	ip_header.src_address, ip_header.dst_address))
         if ip_header.protocol == "ICMP":
+	    offset = ip_header.ihl * 4
             buf = raw_buffer[offset:offset + sizeof(ICMP)]
             icmp_header = ICMP(buf)
         print "ICMP -> Type: %d Code: %d" % (icmp_header.type, icmp_header.code)
